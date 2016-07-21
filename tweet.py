84bf71df6efd62b11e99f2e0126167b39d330e83
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-21 22:09:37 +0100

    Add twitter functionality

--- tweet.py
+++ tweet.py
@@ -1,2 +1,10 @@
+from twitter import *
+
 def send_tweet(text):
-    pass
+    config = {}
+    execfile("config.py", config)
+
+    twitter = Twitter(
+        auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
+
+    results = twitter.statuses.update(status = text)
