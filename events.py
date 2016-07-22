184af8a6cac1fcff4b4a5b04ca69a05bfe11a9f2
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-22 11:58:19 +0100

    Added hyperbolic crochet workshop with placeholder time

--- events.py
+++ events.py
@@ -1,3 +1,4 @@
 events = [
-          ('Maths puzzle evening',(2016,8,5,20,0,0),(2016,8,5,23,59,0))
+          ('Maths puzzle evening',(2016,8,5,20,0,0),(2016,8,5,23,59,0)),
+          ('Hyperbolic crochet workshop',(2016,8,6,10,0,0),(2016,8,5,13,00,0))
          ]
