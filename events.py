e43a6708a745def25522d3702d8b61348b20a249
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-27 20:10:21 +0100

    add yoga

--- events.py
+++ events.py
@@ -2,14 +2,14 @@ events = [
           # Friday
           ('Maths Jam',(2016,8,5,20,0,0),(2016,8,5,23,59,0),"An evening of maths discussion and puzzles, like Maths Jam (http://mathsjam.com). With regular trips to the bar to buy more beer."),
           # Saturday
+          ('Morning yoga',(2016,8,6,9,0,0),(2016,8,6,10,00,0)),
           ('Flexagon making',(2016,8,6,20,0,0),(2016,8,6,21,00,0),"We will make flexagons. This won't take an hour, so drop in whenever."),
           # Sunday
+          ('Morning yoga',(2016,8,7,9,0,0),(2016,8,7,10,00,0)),
           ('Hyperbolic crochet workshop',(2016,8,7,12,0,0),(2016,8,7,14,00,0),"We will teach you how to make hyperbolic surfaces using crochet. No prior ability to crochet required.")
          ]
 
 # unscheduled so far (ignored from ical feed)
 unsc = [
-          ('Giant Cards Against Humanity game',(2016,8,6,10,0,0),(2016,8,6,13,00,0)),
-          ('Morning yoga',(2016,8,6,8,0,0),(2016,8,6,9,00,0)),
-          ('Morning yoga',(2016,8,7,8,0,0),(2016,8,7,9,00,0))
+          ('Giant Cards Against Humanity game',(2016,8,6,10,0,0),(2016,8,6,13,00,0))
        ]
