68e0aa2ca4aeb10f012afe0d0236d094ff89058e
MScroggs <matthew.w.scroggs@gmail.com>
2016-07-27 20:28:23 +0100

    Edit event times

--- events.py
+++ events.py
@@ -1,12 +1,12 @@
 events = [
           # Friday
           ('Maths Jam',(2016,8,5,20,0,0),(2016,8,5,23,59,0),"An evening of maths discussion and puzzles, like Maths Jam (http://mathsjam.com). With regular trips to the bar to buy more beer."),
+          ('Hyperbolic crochet workshop',(2016,8,5,20,0,0),(2016,8,5,22,00,0),"We will teach you how to make hyperbolic surfaces using crochet. No prior ability to crochet required."),
+          ('Flexagon making',(2016,8,5,20,0,0),(2016,8,5,21,00,0),"We will make flexagons. This won't take an hour, so drop in whenever."),
           # Saturday
           ('Morning yoga',(2016,8,6,9,0,0),(2016,8,6,10,00,0)),
-          ('Flexagon making',(2016,8,6,20,0,0),(2016,8,6,21,00,0),"We will make flexagons. This won't take an hour, so drop in whenever."),
           # Sunday
-          ('Morning yoga',(2016,8,7,9,0,0),(2016,8,7,10,00,0)),
-          ('Hyperbolic crochet workshop',(2016,8,7,12,0,0),(2016,8,7,14,00,0),"We will teach you how to make hyperbolic surfaces using crochet. No prior ability to crochet required.")
+          ('Morning yoga',(2016,8,7,9,0,0),(2016,8,7,10,00,0))
          ]
 
 # unscheduled so far (ignored from ical feed)
