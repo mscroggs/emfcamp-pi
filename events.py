ccb21f9415c5c2aa5df88df1aa96914b5f6c9dc4
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-01 12:04:11 +0100

    more non maths disclaimers

--- events.py
+++ events.py
@@ -4,13 +4,13 @@ events = [
           ('Hyperbolic crochet workshop',(2016,8,5,20,15,0),(2016,8,5,22,00,0),"We will teach you how to make hyperbolic surfaces using crochet. No prior ability to crochet required."),
           ('Flexagon making',(2016,8,5,20,30,0),(2016,8,5,23,00,0),"We will make flexagons. Drop in any time this evening, they don't take very long."),
           # Saturday
-          ('Morning yoga',(2016,8,6,9,0,0),(2016,8,6,10,00,0)),
+          ('Morning yoga',(2016,8,6,9,0,0),(2016,8,6,10,00,0),"Disclaimer: not maths"),
           ('First aid workshop',(2016,8,6,10,30,0),(2016,8,6,11,00,0),"We will show you how to perform CPR on a dummy. This is for fun and is obviously not actual first aid training.\nDisclaimer: not maths"),
           ('Suturing (stitching) workshop',(2016,8,6,11,00,0),(2016,8,6,11,20,0),"Learn how to put medical style stiches into pork belly. Useful for fixing clothes or multiple bird roasts. This is for fun and is obviously not actual medical training.\nDisclaimer: not maths"),
           ('Suturing (stitching) workshop',(2016,8,6,11,20,0),(2016,8,6,11,40,0),"Learn how to put medical style stiches into pork belly. Useful for fixing clothes or multiple bird roasts. This is for fun and is obviously not actual medical training.\nDisclaimer: not maths"),
           ('Suturing (stitching) workshop',(2016,8,6,11,40,0),(2016,8,6,12,00,0),"Learn how to put medical style stiches into pork belly. Useful for fixing clothes or multiple bird roasts. This is for fun and is obviously not actual medical training.\nDisclaimer: not maths"),
           # Sunday
-          ('Morning yoga',(2016,8,7,9,0,0),(2016,8,7,10,00,0)),
+          ('Morning yoga',(2016,8,7,9,0,0),(2016,8,7,10,00,0),"Disclaimer: not maths"),
           ('The Maths of poker',(2016,8,7,10,0,0),(2016,8,7,11,00,0),"Basic mathematics and game theory of poker for beginners. We can have a game at the end if people want."),
           ('Cauchy\'s functional equation',(2016,8,7,11,0,0),(2016,8,7,11,20,0),"An interactive session investigating solutions to Cauchy's functional equation (f(x+y)=f(x)+f(y)).")
          ]
