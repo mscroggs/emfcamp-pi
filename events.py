2ab71acdded7a908fb4d752b3927b7a4cde9a33d
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-01 11:49:21 +0100

    Add more events

--- events.py
+++ events.py
@@ -5,9 +5,14 @@ events = [
           ('Flexagon making',(2016,8,5,20,30,0),(2016,8,5,21,00,0),"We will make flexagons. This won't take an hour, so drop in whenever."),
           # Saturday
           ('Morning yoga',(2016,8,6,9,0,0),(2016,8,6,10,00,0)),
-          ('The Maths of poker',(2016,8,6,10,0,0),(2016,8,6,11,00,0),"Basic mathematics and game theory of poker for beginners. We can have a game at the end if people want."),
+          ('First aid workshop',(2016,8,6,10,30,0),(2016,8,6,11,00,0),"We will show you how to perform CPR on a dummy.\nDisclaimer: Not maths"),
+          ('Suturing workshop',(2016,8,6,11,00,0),(2016,8,6,11,20,0),"Learn how to put stiches into pork belly. Useful for fixing clothes or multiple bird roasts.\nDisclaimer: Not maths"),
+          ('Suturing workshop',(2016,8,6,11,20,0),(2016,8,6,11,40,0),"Learn how to put stiches into pork belly. Useful for fixing clothes or multiple bird roasts.\nDisclaimer: Not maths"),
+          ('Suturing workshop',(2016,8,6,11,40,0),(2016,8,6,12,00,0),"Learn how to put stiches into pork belly. Useful for fixing clothes or multiple bird roasts.\nDisclaimer: Not maths"),
           # Sunday
-          ('Morning yoga',(2016,8,7,9,0,0),(2016,8,7,10,00,0))
+          ('Morning yoga',(2016,8,7,9,0,0),(2016,8,7,10,00,0)),
+          ('The Maths of poker',(2016,8,7,10,0,0),(2016,8,7,11,00,0),"Basic mathematics and game theory of poker for beginners. We can have a game at the end if people want."),
+          ('Cauchy\'s functional equation',(2016,8,7,11,0,0),(2016,8,7,11,20,0),"An interactive session investigation solutions to Cauchy's functional equation (f(x+y)=f(x)+f(y)).")
          ]
 
 # unscheduled so far (ignored from ical feed)
