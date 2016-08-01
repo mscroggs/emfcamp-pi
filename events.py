ade705c4070e8357abe953974efe8c693dd325a2
MScroggs <matthew.w.scroggs@gmail.com>
2016-08-01 06:57:35 +0100

    Make events appear on schedule (not at same time)

--- events.py
+++ events.py
@@ -1,8 +1,9 @@
 events = [
           # Friday
-          ('Maths Jam',(2016,8,5,20,0,0),(2016,8,5,23,59,0),"An evening of maths discussion and puzzles, like Maths Jam (http://mathsjam.com). With regular trips to the bar to buy more beer."),
-          ('Hyperbolic crochet workshop',(2016,8,5,20,15,0),(2016,8,5,22,00,0),"We will teach you how to make hyperbolic surfaces using crochet. No prior ability to crochet required."),
-          ('Flexagon making',(2016,8,5,20,30,0),(2016,8,5,21,00,0),"We will make flexagons. This won't take an hour, so drop in whenever."),
+          ('Maths Jam',(2016,8,5,20,0,0),(2016,8,5,20,15,0),"An evening of maths discussion and puzzles, like Maths Jam (http://mathsjam.com). With regular trips to the bar to buy more beer."),
+          ('Maths Jam continues',(2016,8,5,22,0,0),(2016,8,5,23,59,0),"An evening of maths discussion and puzzles, like Maths Jam (http://mathsjam.com). With regular trips to the bar to buy more beer."),
+          ('Hyperbolic crochet workshop',(2016,8,5,20,30,0),(2016,8,5,22,00,0),"We will teach you how to make hyperbolic surfaces using crochet. No prior ability to crochet required."),
+          ('Flexagon making',(2016,8,5,20,15,0),(2016,8,5,20,30,0),"We will make flexagons. This won't take an hour, so drop in whenever."),
           # Saturday
           ('Morning yoga',(2016,8,6,9,0,0),(2016,8,6,10,00,0)),
           # Sunday
